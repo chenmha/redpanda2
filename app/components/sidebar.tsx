bd032553c9a965d97ca272ee3238eee96d37ba0c
chenmha <115022255+chenmha@users.noreply.github.com>
2024-04-16 16:20:05 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           高门学术AI
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          考一张驾照，而不是和AI赛跑。
+          考一张驾照，而不是和AI这台跑车赛跑。
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
